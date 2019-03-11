76beff85829ae8c3523ca89f8db20fb6cb1764e3
Moritzjenny <moritzjenny@hotmail.com>
2019-03-11 18:24:42 +0100

    user test

--- src/test/java/ch/uzh/ifi/seal/soprafs19/service/UserServiceTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs19/service/UserServiceTest.java
@@ -47,7 +47,7 @@ public class UserServiceTest {
         User createdUser = userService.createUser(testUser);
 
         Assert.assertNotNull(createdUser.getToken());
-        Assert.assertEquals(createdUser.getStatus(),UserStatus.ONLINE);
+        Assert.assertEquals(createdUser.getStatus(),UserStatus.OFFLINE);
         Assert.assertEquals(createdUser, userRepository.findByToken(createdUser.getToken()));
     }
 }
